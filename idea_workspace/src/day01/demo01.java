3e8ee21ac1a0255d7ebfefe41735c5b03fbb01f7
snowword <826190462@qq.com>
2019-02-28 11:51:02 +0800

    Signed-off-by: snowword <826190462@qq.com>

---
+++ idea_workspace/src/day01/demo01.java
@@ -0,0 +1,14 @@
+package day01;
+
+public class demo01 {
+    public static void main(String[] args) {
+        int a = 60;
+        int b = 12;
+        int c = 0;
+        c=a&b;
+        c=a|b;
+        c=a^b;
+        System.out.println(c);
+    }
+
+}
